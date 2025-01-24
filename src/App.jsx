caff96269c99d53322edf382e32900ceef249930
Raj Soren <7rajkumarsoren@gmail.com>
2025-01-24 23:36:43 +0530

    code change

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-const Card = ({ title }) => {
-  return (
-    <div>
-      <h3>{title}</h3>
-    </div>
-  );
-};
+import React from 'react'
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h2>Functional Arrow Component</h2>
-      <Card title="Stars Wars" />
-      <Card title="Avatar" />
-      <Card title="The Lion King" />
-    </div>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App

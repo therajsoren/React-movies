6445a3f2feab5fd16e86c0be3c6ab5d5c319a3dd
Raj Soren <7rajkumarsoren@gmail.com>
2025-01-26 04:17:21 +0530

    added image

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import React from 'react';
-import heroImage from './assets/hero.png';
+import React from 'react'
 
 const App = () => {
-  return (
-   <main>
-    <div className='pattern' />
-    <div className='wrapper'/>
-    <header>
-      {/* <img src='./assets/hero.png'/> not working */}
-      <img src={heroImage}/>
-      
-      <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
-    </header>
+    return (
+        <main>
+            <div className='pattern' />
 
-    <p>Search</p>
-   </main>
-  )
+            <div className='wrapper'>
+                <header>
+                    <img src= '../public/hero.png' alt='hero' />
+                    <h1>Find <span className='text-gradient'>Movies</span>You'll Enjoy
+                    Without the Hassle</h1>
+                </header>
+            </div>
+        </main>
+    )
 }
-
-
 export default App

de672b721ab981f7d2a14a6211f94e93844ff15d
Raj Soren <7rajkumarsoren@gmail.com>
2025-01-26 04:43:57 +0530

    added search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-
+import Search from './components/Search.jsx'
+import {useState} from "react";
 const App = () => {
+
+    const [searchTerm, setSearchTerm] = useState('');
     return (
         <main>
             <div className='pattern' />
 
             <div className='wrapper'>
                 <header>
-                    <img src= '../public/hero.png' alt='hero' />
+                    <img src= '../hero.png' alt='hero' />
                     <h1>Find <span className='text-gradient'>Movies</span>You'll Enjoy
                     Without the Hassle</h1>
                 </header>
+            <Search searchTerm = {searchTerm} setSearchTerm = {setSearchTerm} />
             </div>
         </main>
     )
---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+const Search = ({searchTerm,setSearchTerm}) => {
+    return (
+        <div className='search'>
+            <div>
+                <img src='/search.svg' alt='Search'/>
+
+                <input type='text' placeholder='Search through thousands of movies'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)} />
+            </div>
+        </div>
+    )
+}
+export default Search
